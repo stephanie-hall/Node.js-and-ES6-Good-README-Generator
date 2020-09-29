6554e166a6786dea7d21419678a6f555c06a5011
Stephanie Hall <Stephaniehall41@gmail.com>
2020-09-28 21:39:17 -0400

    add gif

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# Node.js-and-ES6-Good-README-Generator
+# Node.js and ES6 Good ReadMe Generator
+
+    ## Description
+    This app builds a "good" README file after the user answers a few inquirer prompts. 
+    ## Table of Contents 
+    1. [Installation](#installation) 
+    2. [Usage](#usage) 
+    3. [Contribution](#contribution) 
+    4. [License](#license)
+    5. [Tests](#tests) 
+    6. [Questions](#questions) 
+
+## Installation
+
+    npm i 
+
+## Usage
+
+    The code is designed to greate a "good" readme file in the terminal. 
+
+## Contribution
+
+    I did 
+
+## License
+
+    undefined
+
+## Tests
+
+    none 
+
+## Questions
+
+    GitHub: stephanie-hall 
+    Email: stephaniehall41@gmail.com 
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,27 +2,34 @@
 function generateMarkdown(data) {
     return `# ${data.projectTitle} 
     ## Description
-    ${data.description} 
+      ${data.description} 
+      
     ## Table of Contents 
-    1. [Installation](#installation) 
-    2. [Usage](#usage) 
-    3. [Contribution](#contribution) 
-    4. [License](#license)
-    5. [Tests](#tests) 
-    6. [Questions](#questions) 
+      1. [Installation](#installation) 
+      2. [Usage](#usage) 
+      3. [Contribution](#contribution) 
+      4. [License](#license)
+      5. [Tests](#tests) 
+      6. [Questions](#questions) 
+
     ## Installation 
-    ${data.installation} 
+      ${data.installation}
+
     ## Usage 
-    ${data.usage} 
+      ${data.usage}
+
     ## Contribution 
-    ${data.contribution} 
+      ${data.contribution}
+
     ## License 
-    ${data.License}
+      ${data.License}
+
     ## Tests 
-    ${data.tests} 
+      ${data.tests}
+
     ## Questions 
-    GitHub: ${data.github} 
-    Email: ${data.email} 
+      GitHub: ${data.github}
+      Email: ${data.email}
   `;
 }
   
